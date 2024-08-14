960d5be3366c4d106b0dbcfcb556ca03b45da850
unknown <lucasvieirasouzaa@gmail.com>
2024-08-13 21:55:28 -0300

    Responsivity update

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 //Versão do Sistema
-let version = "0.214v";
+let version = "0.216v";
 document.getElementById('systemVersion').textContent = version;
 
 //Redes-Sociais
