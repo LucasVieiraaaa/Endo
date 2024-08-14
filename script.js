7b286905b9e4e10e41721084965c61f1ec86253a
unknown <lucasvieirasouzaa@gmail.com>
2024-08-14 19:26:28 -0300

    Responsivity Update

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 //Versão do Sistema
-let version = "0.22v";
+let version = "0.250v";
 document.getElementById('systemVersion').textContent = version;
 
 //Redes-Sociais
