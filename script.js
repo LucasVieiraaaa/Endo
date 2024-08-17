b4322cf92ac10b3c6335193527d8f553e2e8ac42
unknown <lucasvieirasouzaa@gmail.com>
2024-08-17 01:22:38 -0300

    Update 0.34v

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 //Versão do Sistema
-let version = "0.3v";
+let version = "0.34v";
 document.getElementById('systemVersion').textContent = version;
 
 //Redes-Sociais
