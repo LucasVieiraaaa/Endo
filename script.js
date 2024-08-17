5926076e1edadd6c03cb5e230bd4ac1812cb5aa6
unknown <lucasvieirasouzaa@gmail.com>
2024-08-17 19:02:17 -0300

    Update

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 //Versão do Sistema
-let version = "0.385v";
+let version = "0.39v";
 document.getElementById('systemVersion').textContent = version;
 
 //Redes-Sociais
