55cf7d0aae5a45938feebf67be234f574c559bf4
unknown <lucasvieirasouzaa@gmail.com>
2024-08-26 00:16:29 -0300

    Forgot someting

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 //Versão do Sistema
-let version = "0.5v";
+let version = "0.52v";
 document.getElementById('systemVersion').textContent = version;
 
 //Redes-Sociais
